77b865f7c1f1d66a7ee900a66f11f8d4b9808437
justin long <justinxlong93@gmail.com>
2024-06-17 11:37:30 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Linq_Exercise
+    
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoGames = new List<string>()
+            {
+                "call of duty", "paladins", "osrs", "hghghghghghghghghghg", "a", "ab"
+
+            };
+
+            var videoGameLength = from games in videoGames
+                                  where games.Length > 0
+                                  orderby games.Length 
+                                  select games;
+
+            foreach (var i in videoGameLength)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
